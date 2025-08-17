fe22a89066e3a733cf5734a6f902fb1697345b8c
seb <contact@smaloron.eu>
2025-08-17 16:34:27 +0200

    build for github pages

--- src/firebase/useFirestore.js
+++ src/firebase/useFirestore.js
@@ -3,6 +3,9 @@ import { collection, addDoc, serverTimestamp } from "firebase/firestore";
 
 // Save poll answers to Firestore
 export async function savePoll(answers) {
+
+
+
     await addDoc(collection(db, "pollAnswers"), {
         answers,
         createdAt: serverTimestamp()
