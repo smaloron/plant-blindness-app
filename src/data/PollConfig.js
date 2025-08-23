cb82e90a3b891617970baaf0d2b4cbc6e9e13341
seb <contact@smaloron.eu>
2025-08-23 20:30:07 +0200

    Fix the links and build the app

--- src/data/PollConfig.js
+++ src/data/PollConfig.js
@@ -1,49 +1,49 @@
 // For question 1 : favorite seed
 const seedsList = [
-    { id: 1, name: "Althéa", img: "public/images/1/althea.jpg" },
-    { id: 2, name: "Arbouse", img: "public/images/1/arbouse2.jpg" },
-    { id: 3, name: "Baobab", img: "public/images/1/baobab.jpg" },
-    { id: 4, name: "Cacao", img: "public/images/1/cacao.jpg" },
-    { id: 5, name: "chataigne d'eau", img: "public/images/1/chataigne_d_eau.jpg" },
-    { id: 6, name: "Enterobolium", img: "public/images/1/enterolobium.jpg" },
-    { id: 7, name: "Euphorbe", img: "public/images/1/euphorbe.jpg" },
-    { id: 8, name: "Hévéa", img: "public/images/1/Hevea.jpg" },
-    { id: 9, name: "Lilas", img: "public/images/1/lilas.jpg" },
-    { id: 10, name: "Lithops", img: "public/images/1/lithops.jpg" },
-    { id: 11, name: "Lithops fermé", img: "public/images/1/lithops-ferme.jpg" },
-    { id: 12, name: "Samares d'érable", img: "public/images/1/samares_erables.jpg" },
-    { id: 13, name: "Sauge", img: "public/images/1/sauge.jpg" },
-    { id: 14, name: "Souci", img: "public/images/1/Souci.jpg" },
-    { id: 15, name: "Tabac", img: "public/images/1/Tabac.jpg" },
+    { id: 1, name: "Althéa", img: "/images/1/althea.jpg" },
+    { id: 2, name: "Arbouse", img: "/images/1/arbouse2.jpg" },
+    { id: 3, name: "Baobab", img: "/images/1/baobab.jpg" },
+    { id: 4, name: "Cacao", img: "/images/1/cacao.jpg" },
+    { id: 5, name: "chataigne d'eau", img: "/images/1/chataigne_d_eau.jpg" },
+    { id: 6, name: "Enterobolium", img: "/images/1/enterolobium.jpg" },
+    { id: 7, name: "Euphorbe", img: "/images/1/euphorbe.jpg" },
+    { id: 8, name: "Hévéa", img: "/images/1/Hevea.jpg" },
+    { id: 9, name: "Lilas", img: "/images/1/lilas.jpg" },
+    { id: 10, name: "Lithops", img: "/images/1/lithops.jpg" },
+    { id: 11, name: "Lithops fermé", img: "/images/1/lithops-ferme.jpg" },
+    { id: 12, name: "Samares d'érable", img: "/images/1/samares_erables.jpg" },
+    { id: 13, name: "Sauge", img: "/images/1/sauge.jpg" },
+    { id: 14, name: "Souci", img: "/images/1/Souci.jpg" },
+    { id: 15, name: "Tabac", img: "/images/1/Tabac.jpg" },
 ];
 
 // For question 3 : recognized seed
 const recognizedSeedList = [
-    { id: 101, name: "Arachide", img: "public/images/3/arachide.jpg" },
-    { id: 102, name: "Café", img: "public/images/3/Cafe.jpg" },
-    { id: 103, name: "Dans le Zome", img: "public/images/3/dans-le-zome3.jpg" },
-    { id: 104, name: "Églantine", img: "public/images/3/Eglantine.jpg" },
-    { id: 105, name: "Haricot d'eau", img: "public/images/3/haricot.jpg" },
-    { id: 106, name: "Samares d'érable", img: "public/images/3/samares_erables.jpg" },
+    { id: 101, name: "Arachide", img: "/images/3/arachide.jpg" },
+    { id: 102, name: "Café", img: "/images/3/Cafe.jpg" },
+    { id: 103, name: "Dans le Zome", img: "/images/3/dans-le-zome3.jpg" },
+    { id: 104, name: "Églantine", img: "/images/3/Eglantine.jpg" },
+    { id: 105, name: "Haricot d'eau", img: "/images/3/haricot.jpg" },
+    { id: 106, name: "Samares d'érable", img: "/images/3/samares_erables.jpg" },
 ];
 
 // For question 5 : Three words about a seed
 const threeWordsAboutASeedList = [
-    { id: 201, name: "Althéa", img: "public/images/5/althea.jpg" },
-    { id: 202, name: "Chataigne d'eau", img: "public/images/5/chataigne_d_eau.jpg" },
-    { id: 203, name: "Euphorbe", img: "public/images/5/euphorbe.jpg" },
-    { id: 204, name: "Pépin de raisin", img: "public/images/5/Pepin-raisin2.png" },
-    { id: 205, name: "Sauge", img: "public/images/5/Sauge.JPG" },
-    { id: 206, name: "Tabac", img: "public/images/5/tabac.JPG" },
+    { id: 201, name: "Althéa", img: "/images/5/althea.jpg" },
+    { id: 202, name: "Chataigne d'eau", img: "/images/5/chataigne_d_eau.jpg" },
+    { id: 203, name: "Euphorbe", img: "/images/5/euphorbe.jpg" },
+    { id: 204, name: "Pépin de raisin", img: "/images/5/Pepin-raisin2.png" },
+    { id: 205, name: "Sauge", img: "/images/5/Sauge.JPG" },
+    { id: 206, name: "Tabac", img: "/images/5/tabac.JPG" },
 ]
 // For question 7 : the fake/imaginary seed question
 const fakeSeedsList = [
-    { id: 301, name: "Campanule", img: "public/images/7/Campanule.jpg", isFake: false },
-    { id: 302, name: "Cyprus", img: "public/images/7/cyprus.jpg", isFake: false },
-    { id: 303, name: "Euonymus", img: "public/images/7/Euonymus.jpg", isFake: false },
-    { id: 304, name: "Lavatere", img: "public/images/7/lavatere.jpg", isFake: false },
-    { id: 305, name: "Lychnis", img: "public/images/7/lychnis.jpg", isFake: true },
-    { id: 306, name: "Tetragone", img: "public/images/7/tetragone.jpg", isFake: true },
+    { id: 301, name: "Campanule", img: "/images/7/Campanule.jpg", isFake: false },
+    { id: 302, name: "Cyprus", img: "/images/7/cyprus.jpg", isFake: false },
+    { id: 303, name: "Euonymus", img: "/images/7/Euonymus.jpg", isFake: false },
+    { id: 304, name: "Lavatere", img: "/images/7/lavatere.jpg", isFake: false },
+    { id: 305, name: "Lychnis", img: "/images/7/lychnis.jpg", isFake: true },
+    { id: 306, name: "Tetragone", img: "/images/7/tetragone.jpg", isFake: true },
 ];
 
 // Main configuration array
